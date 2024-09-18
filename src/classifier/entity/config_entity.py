391555692a51f64b72bb537c3e829d1f50df7df9
MalavikaGowthaman <malavikag115@gmail.com>
2024-09-18 12:26:55 -0400

    Base model added

--- src/classifier/entity/config_entity.py
+++ src/classifier/entity/config_entity.py
@@ -46,3 +46,15 @@ class EvaluationConfig:
     mlflow_uri: str
     params_image_size: list
     params_batch_size: int
+    
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
